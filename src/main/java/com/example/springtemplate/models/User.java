0e5d89ad8cf73cef266a83676469d9a80872bec4
Tanveer Singh <tanveer7537@gmail.com>
2021-12-01 20:32:29 -0500

    Implemented User and refactored

--- src/main/java/com/example/springtemplate/models/User.java
+++ src/main/java/com/example/springtemplate/models/User.java
@@ -12,31 +12,86 @@ public class User {
     private String lastName;
     private String username;
     private String password;
-    private String profilePicture;
-    private String handle;
+    private String dateOfBirth;
+    private String created;
+    private String updated;
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getProfilePicture() { return profilePicture; }
-    public void setProfilePicture(String profilePicture) { this.profilePicture = profilePicture; }
-    public String getHandle() { return handle; }
-    public void setHandle(String handle) { this.handle = handle; }
-
-    public User(String username, String password, String first_name, String last_name, String profile_picture) {
-        this.username = username;
-        this.password = password;
-        this.firstName = first_name;
-        this.lastName = last_name;
-        this.profilePicture = profile_picture;
+    public Integer getId() {
+        return id;
     }
 
-    public User() {}
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public User(Integer id, String firstName, String lastName, String username, String password, String dateOfBirth, String created, String updated) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public User(){
+
+    }
 }
